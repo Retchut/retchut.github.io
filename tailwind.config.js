db415f508be08cd21caf1b634f104c06ca7b6603
Mário Travassos <retchut@protonmail.ch>
2023-08-25 23:10:21 +0100

    chore: add project fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,34 @@
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		colors: {
-			background: "#0C1618",
-			titles: "#004643",
-			accent: "#D1AC00",
-			main: "#FAF4D3",
-			secondary: "#F6BE9A",
+		extend: {
+			colors: {
+				background: "#0C1618",
+				titles: "#004643",
+				accent: "#D1AC00",
+				main: "#FAF4D3",
+				secondary: "#F6BE9A",
 
-			background1: "#040510",
-			titles1: "#6392d8",
-			accent1: "#9ec0dc",
-			main1: "#e5e6df",
-			secondary1: "#d1d3d9",
-			other1: "#737681",
+				background1: "#040510",
+				titles1: "#6392d8",
+				accent1: "#9ec0dc",
+				main1: "#e5e6df",
+				secondary1: "#d1d3d9",
+				other1: "#737681",
 
-			background2: "#25283b",
-			titles2: "#6392d8",
-			accent2: "#91958b",
-			main2: "#e5e6df",
-			secondary2: "#95cffb",
-			other2: "#737681"
-		},
-		extend: {}
+				background2: "#25283b",
+				titles2: "#6392d8",
+				accent2: "#91958b",
+				main2: "#e5e6df",
+				secondary2: "#95cffb",
+				other2: "#737681"
+			},
+			fontFamily: {
+				oswald: ["Oswald", "sans-serif"],
+				"roboto-condensed": ["Roboto Condensed", "sans-serif"],
+				"babas-neue": ["Bebas Neue", "sans-serif"]
+			}
+		}
 	},
 	plugins: []
 };
