50f335ed0a8ef025cef7ab35d96cad367f2ce45a
Mário Travassos <retchut@protonmail.ch>
2023-08-26 00:23:37 +0100

    fix: fix navbar hamburguer styles

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,14 +2,14 @@
 </script>
 
 <nav class="flex justify-between items-center px-16 py-8">
-    <h1 class="text-3xl font-babas-neue">MÁRIO TRAVASSOS</h1>
-    <div class="bg-background px-3 py-4 rounded-full">
-    <div class="space-y-2">
-        <div class="w-8 h-0.5 bg-titles"></div>
-        <div class="w-8 h-0.5 bg-titles"></div>
-        <div class="w-8 h-0.5 bg-titles"></div>
-    </div>
-    </div>
+	<h1 class="text-3xl font-babas-neue">MÁRIO TRAVASSOS</h1>
+	<div class="bg-background px-3 py-4 rounded-full">
+		<div class="space-y-2">
+			<div class="w-8 h-0.5 bg-title"></div>
+			<div class="w-8 h-0.5 bg-title"></div>
+			<div class="w-8 h-0.5 bg-title"></div>
+		</div>
+	</div>
 </nav>
 
 <style>
