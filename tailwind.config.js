2264c034d1bbb35587af8c7f778368141a3c143b
Mário Travassos <retchut@protonmail.ch>
2023-08-26 00:16:14 +0100

    chore: update tailwind styles name

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,20 @@ export default {
 		extend: {
 			colors: {
 				background: "#0C1618",
-				titles: "#004643",
+				title: "#004643",
 				accent: "#D1AC00",
 				main: "#FAF4D3",
 				secondary: "#F6BE9A",
 
 				background1: "#040510",
-				titles1: "#6392d8",
+				title1: "#6392d8",
 				accent1: "#9ec0dc",
 				main1: "#e5e6df",
 				secondary1: "#d1d3d9",
 				other1: "#737681",
 
 				background2: "#25283b",
-				titles2: "#6392d8",
+				title2: "#6392d8",
 				accent2: "#91958b",
 				main2: "#e5e6df",
 				secondary2: "#95cffb",
