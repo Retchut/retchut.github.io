cfb2d564e38fe25ad2550535c996225f973fb32a
Mário Travassos <retchut@protonmail.ch>
2023-08-25 23:11:07 +0100

    feat: implement simple navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 </script>
 
-<section>
-    
-</section>
+<nav class="flex justify-between items-center px-16 py-8">
+    <h1 class="text-3xl font-babas-neue">MÁRIO TRAVASSOS</h1>
+    <div class="bg-background px-3 py-4 rounded-full">
+    <div class="space-y-2">
+        <div class="w-8 h-0.5 bg-titles"></div>
+        <div class="w-8 h-0.5 bg-titles"></div>
+        <div class="w-8 h-0.5 bg-titles"></div>
+    </div>
+    </div>
+</nav>
 
 <style>
 </style>
