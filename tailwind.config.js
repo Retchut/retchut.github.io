fbbe174e7e00fe40967e16391535da702b11d86f
Mário Travassos <retchut@protonmail.ch>
2023-08-25 22:10:17 +0100

    chore: add test palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ export default {
 			titles: "#004643",
 			accent: "#D1AC00",
 			main: "#FAF4D3",
-			secondary: "#F6BE9A"
+			secondary: "#F6BE9A",
+
+			background1: "#040510",
+			titles1: "#6392d8",
+			accent1: "#9ec0dc",
+			main1: "#e5e6df",
+			secondary1: "#d1d3d9",
+			other1: "#737681",
+
+			background2: "#25283b",
+			titles2: "#6392d8",
+			accent2: "#91958b",
+			main2: "#e5e6df",
+			secondary2: "#95cffb",
+			other2: "#737681"
 		},
 		extend: {}
 	},
