12cad1d7041b8e92e83597a442572f016031c27c
Mário Travassos <retchut@protonmail.ch>
2023-09-12 01:27:25 +0100

    chore: update about text

--- src/lib/Sections/About.svelte
+++ src/lib/Sections/About.svelte
@@ -12,7 +12,7 @@
 	const title = "About me";
 	const paragraphs = [
 		"My name is Mário Travassos, an aspiring Software Developer from Portugal. I'm deeply interested in **Game Design**, **Game Development** and **Web Development**.",
-		"When I'm not studying or working in the aforementioned areas, you may find me reparing and/or tinkering with electronics and clockwork, automating simple tasks I could have done myself by hand in 30 seconds, playing video games or trading card games, or consuming all kinds of media.",
+		"When I'm not studying or working in the aforementioned areas, you may find me **reparing** and/or **tinkering** with electronics and clockwork, **automating** simple tasks I could have done myself by hand in 30 seconds, playing video or trading card **games**, or consuming all kinds of media.",
 		"I'm currently attending the 2nd year of a Master's Degree in Informatics and Computer Engineering at FEUP, working on my thesis focused on **optimizing photo-realistic VR experiences for the web**.",
 		"I am also the current President of the **Nucleus of Computer Graphics and  Multimedia (NCGM)** of FEUP, a youth association focused on Computer Graphics, Human-Computer Interaction and Game Development. We host a multitude of events, but our biggest one is the Game Dev Meet @Porto."
 	];
