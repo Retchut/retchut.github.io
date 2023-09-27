1f166bba76985ba14aa8af1b57114163890ef69b
Mário Travassos <retchut@protonmail.ch>
2023-09-27 12:13:28 +0100

    feat: implement link parsing on styled text component

--- src/lib/Components/Text/StyledParagraph.svelte
+++ src/lib/Components/Text/StyledParagraph.svelte
@@ -4,6 +4,7 @@
 
 	- //text// - *text*
 	- \*\*text\*\* - **text**
+	- ||label[www.example.com]|| - [label](https://www.example.com)
 
 		@param content - string - string to be parsed and displayed
 -->
@@ -13,18 +14,34 @@
 	//	sadly can't use capturing groups to make sure we capture the same start and end,
 	//	as any capture group would get picked up and be spliced into the str.split return array
 	// 	so we need to double check the validity over at (#1)
-	const parsedParagraph = content.split(/((?:\*\*|\/\/).*?(?:\*\*|\/\/))/);
+	const parsedParagraph = content.split(/((?:\*\*|\/\/|\|\|).*?(?:\*\*|\/\/|\|\|))/);
+
+	const getTextLinkPair = (content: string): string[] => {
+		// will generally return something in the form of [ "label", "https://www.url.to.something", "" ]
+		let splitContent = content.split(/\[(.*?)\]/);
+
+		// check if [label, url, emptystr] wasn't returned
+		if (splitContent.length !== 3) return ["", ""];
+
+		return splitContent.slice(0, 2);
+	};
+
+	console.log(parsedParagraph);
 </script>
 
 <p class="text-base pb-1">
 	{#each parsedParagraph as token}
 		{@const tokenStart = token.slice(0, 2)}
 		{@const tokenEnd = token.slice(-2)}
+		{@const content = token.slice(2, -2)}
 		<!-- (#1) -->
 		{#if tokenStart === "**" && tokenEnd === "**"}
-			<span class="font-bold">{token.slice(2, -2)}</span>
+			<span class="font-bold">{content}</span>
 		{:else if tokenStart === "//" && tokenEnd === "//"}
-			<span class="italic">{token.slice(2, -2)}</span>
+			<span class="italic">{content}</span>
+		{:else if tokenStart === "||" && tokenEnd === "||"}
+			{@const textLinkPair = getTextLinkPair(content)}
+			<a href={"https://" + textLinkPair[1]}>{textLinkPair[0]}</a>
 		{:else}
 			{token}
 		{/if}
