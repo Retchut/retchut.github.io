c679960da2726c5dbae4ec8a1ec6d80184a38b94
Mário Travassos <retchut@protonmail.ch>
2023-10-06 14:19:17 +0100

    feat: implement boxes in contact section

--- src/lib/Sections/Contacts.svelte
+++ src/lib/Sections/Contacts.svelte
@@ -3,20 +3,29 @@
 	This component is the webpage's Contacts section.
 -->
 <script lang="ts">
+	import { theme } from "../../utils/stores";
+
 	import PageSection from "../Components/Layout/PageSection.svelte";
 	import TextGroup from "../Components/Text/TextGroup.svelte";
+	import Button from "../Components/Button/Button.svelte";
 
 	import sectionData from "../Assets/Data/Contact.json";
-	import Button from "../Components/Button/Button.svelte";
+
+	let themeVal: number;
+	theme.subscribe((value) => {
+		themeVal = value;
+	});
 
 	const title: string = sectionData["title"] ?? "DefaultTitleTxt";
 	// const subtitle: string = sectionData["subtitle"] ?? "DefaultSubtitleTxt";
 	const paragraphs: string[] = sectionData["paragraphs"] ?? ["DefaultValue"];
 	const contactBtnTxt: string = sectionData["buttonText"] ?? "ContactBtnTxt";
+
+	const boxStyling = "absolute w-full h-full border-4 color-fade-anim";
 </script>
 
 <PageSection>
-	<div class="text-white flex flex-col">
+	<div class="text-white flex flex-col relative">
 		<header>
 			<TextGroup {title} {paragraphs} align="center" />
 		</header>
@@ -25,6 +34,10 @@
 			text={contactBtnTxt}
 			action={() => (window.location.href = "mailto:mariotravassosdev@protonmail.com")}
 		/>
+		<div class="absolute inset-0 m-auto w-2/3 h-[60vh]">
+			<div class="{boxStyling} -inset-4 border-accent{themeVal}"></div>
+			<div class="{boxStyling} inset-4 border-accent{(themeVal + 1) % 4}"></div>
+		</div>
 	</div>
 </PageSection>
 
