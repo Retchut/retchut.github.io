9cd72bc66b3cbaf4a19a8abdd4aa678e528c4542
Mário Travassos <retchut@protonmail.ch>
2023-10-10 18:30:37 +0100

    refactor: remove dom manipulation code on the gradient transition component

--- src/lib/Components/Project/GradientTransition.svelte
+++ src/lib/Components/Project/GradientTransition.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import { onMount } from "svelte";
 	import { get } from "svelte/store";
 	import { theme } from "../../../utils/stores";
-
-	import { addClass, removeClass } from "../../../utils/classUtils";
 
 	export let onRight: boolean = false;
 
@@ -14,51 +11,37 @@
 
 	// this is a little iffy, but we have to get the theme value separately, otherwise we'll update the previous value on every change of the themeVal, which is not intended, as an update to themeVal doesn't necessarily mean we want to reanimate the currently active gradient
 	let prevThemeVal: number = get(theme);
+	// other color change animations take 200ms - defined by tailwind
+	const animDurationMS: number = 500;
 
-	const animDurationMS: number = 200;
-
-	let gradients: HTMLElement[] = [];
-	let visibleGradients: boolean[] = [true, false, false, false];
-
-	const fadeOut = (element: HTMLElement): void => {
-		addClass(element, "fade-out");
-		addClass(element, "opacity-0");
-		removeClass(element, "fade-in");
-		removeClass(element, "opacity-100");
-	};
-
-	const fadeIn = (element: HTMLElement): void => {
-		addClass(element, "fade-in");
-		addClass(element, "opacity-100");
-		removeClass(element, "fade-out");
-		removeClass(element, "opacity-0");
-	};
+	let firstRun = true;
+	let visibleGradients: boolean[] = [false, false, false, false];
 
 	const updateGradients = (currentTheme: number) => {
-		if (gradients.length !== 0) {
-			visibleGradients[prevThemeVal] = !visibleGradients[prevThemeVal];
-			visibleGradients[currentTheme] = !visibleGradients[currentTheme];
-			fadeOut(gradients[prevThemeVal]);
-			fadeIn(gradients[currentTheme]);
+		// unhide default gradient color
+		if (firstRun) {
+			visibleGradients[currentTheme] = true;
+			firstRun = false;
+			return;
 		}
+
+		visibleGradients[prevThemeVal] = !visibleGradients[prevThemeVal];
+		visibleGradients[currentTheme] = !visibleGradients[currentTheme];
 		prevThemeVal = currentTheme;
 	};
 
-	onMount(() => {
-		removeClass(gradients[themeVal], "opacity-0"); // unhide current theme element (they all start hidden)
-	});
-
-	// using a callback because otherwise this snippet would run whenever either the prevThemeVal or the themeVal were updated
+	// using a callback in order to hide prevThemeVal from the svelte compiler
 	$: updateGradients(themeVal);
 </script>
 
 {#each [0, 1, 2, 3] as t}
 	<div
 		style="--anim-duration-ms:{animDurationMS};"
-		bind:this={gradients[t]}
 		class:fade-in={visibleGradients[t]}
+		class:opacity-100={visibleGradients[t]}
 		class:fade-out={!visibleGradients[t]}
-		class="opacity-0 w-full h-full absolute bg-gradient-to-{onRight
+		class:opacity-0={!visibleGradients[t]}
+		class="w-full h-full absolute bg-gradient-to-{onRight
 			? 'l'
 			: 'r'} from-accent{t} from-60% to-background2"
 	></div>
@@ -84,12 +67,12 @@
 	}
 
 	.fade-out {
-		animation: toInvisible calc(var(--anim-duration-ms) * 1ms); /* other color change animations take 200ms - defined by tailwind */
+		animation: toInvisible calc(var(--anim-duration-ms) * 1ms);
 		transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
 	}
 
 	.fade-in {
-		animation: toVisible calc(var(--anim-duration-ms) * 1ms); /* other color change animations take 200ms - defined by tailwind */
+		animation: toVisible calc(var(--anim-duration-ms) * 1ms);
 		transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
 	}
 </style>
--- src/utils/classUtils.ts
+++
@@ -1,9 +0,0 @@
-function addClass(element: HTMLElement, toAdd: string): void {
-	element.classList.add(toAdd);
-}
-
-function removeClass(element: HTMLElement, toRemove: string): void {
-	element.classList.remove(toRemove);
-}
-
-export { addClass, removeClass };
