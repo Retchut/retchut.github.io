3f741f506e3a1040612dd625799f00aaca3bb6b9
Mário Travassos <retchut@protonmail.ch>
2024-01-30 00:29:28 +0000

    fix: fix about page responsivity

--- src/lib/Sections/About.svelte
+++ src/lib/Sections/About.svelte
@@ -4,6 +4,8 @@
 -->
 <script lang="ts">
 	// imports
+	import { currentBreakpoint } from "../../utils/stores";
+
 	import PageSection from "../Components/Layout/PageSection.svelte";
 	import TextGroup from "../Components/Text/TextGroup.svelte";
 
@@ -15,15 +17,25 @@
 
 	const title2: string = sectionData["title2"] ?? "DefaultTitleTxt2";
 	const paragraphs2: string[] = sectionData["paragraphs2"] ?? ["DefaultValue2"];
+
+	let smallBreakpoint: boolean = true;
+	currentBreakpoint.subscribe((value) => {
+		smallBreakpoint = value == "xs" || value == "sm" || value == "md";
+		console.log(value);
+	});
 </script>
 
-<PageSection>
-	<div class="flex justify-between">
-		<header class="w-1/3 text-main">
+<PageSection screenHeight={!smallBreakpoint}>
+	<div
+		class="py-[12.5%] {smallBreakpoint
+			? 'pb-[400px]'
+			: ''} flex flex-col items-center gap-y-8 lg:gap-y-0 lg:items-start lg:flex-row lg:justify-between"
+	>
+		<header class="w-full lg:w-1/3 text-main">
 			<TextGroup {title} {paragraphs} />
 		</header>
-		<div class="w-1/2 flex flex-col text-main">
-			<div class="grow flex justify-end items-start mb-8">
+		<div class="w-full lg:w-1/2 self-end flex flex-col text-main">
+			<div class="grow flex justify-center lg:justify-end items-start mb-8">
 				<img src="/about.jpg" alt="My portrait... again" class="w-[350px] rounded-sm" />
 			</div>
 			<TextGroup title={title2} paragraphs={paragraphs2} />
