e3c15e2800f456bcafa0d9f2b09d471eaf93536a
Mário Travassos <retchut@protonmail.ch>
2024-02-18 00:39:51 +0000

    chore: remove extra trailing spaces in projects classes

--- src/lib/Sections/Projects.svelte
+++ src/lib/Sections/Projects.svelte
@@ -95,7 +95,7 @@
 
 <PageSection screenHeight={false}>
 	<div class="pt-[12.5%]">
-		<header class=" w-full text-main">
+		<header class="w-full text-main">
 			<TextGroup {title} titleSize="6xl" />
 		</header>
 
