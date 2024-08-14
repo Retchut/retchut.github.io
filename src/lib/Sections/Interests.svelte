764cf1ec7ed720695d1d4454e0ef4609c8311ddb
Retchut <mariotravassosdev@protonmail.com>
2024-08-14 02:58:02 +0100

    fix: make interest images scale correctly

--- src/lib/Sections/Interests.svelte
+++ src/lib/Sections/Interests.svelte
@@ -35,7 +35,11 @@
 						<div
 							class="group-hover:brightness-[30%] group-hover:blur-sm duration-200 group-hover:scale-110"
 						>
-							<img src={"/interests/" + entry.imgName + ".jpg"} alt={entry.imgName.split("/")[1]} />
+							<img
+								class="w-full"
+								src={"/interests/" + entry.imgName + ".jpg"}
+								alt={entry.imgName.split("/")[1]}
+							/>
 						</div>
 						<!-- back -->
 						<div
